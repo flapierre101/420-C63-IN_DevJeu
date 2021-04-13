e777514907aef8111e21d75cb2f5a747238b01ae
Caroline Emond Serret <caroline.emond.serret@gmail.com>
2021-04-13 17:10:16 -0400

    Player collision with obstacles now functional

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,91 +1,110 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public enum Animation
+  public enum Animation
+  {
+    Idle,
+    Walk,
+    Attack_BT,
+  }
+
+  public Health Health { get; private set; }
+  private FacingController FacingController;
+  private Animator Animator;
+  private Animation _currentAnimation;
+  private Flash Flash;
+
+  public Animation CurrentAnimation
+  {
+    get { return _currentAnimation; }
+    set
     {
-        Idle,
-        Walk,
-        Attack_BT,
+      _currentAnimation = value;
+      UpdateAnimations();
     }
-
-    public Health Health { get; private set; }
-    private FacingController FacingController;
-    private Animator Animator;
-    private Animation _currentAnimation;
-    private Flash Flash;
-
-    public Animation CurrentAnimation
+  }
+  public string AnimationName
+  {
+    get
     {
-        get { return _currentAnimation; }
-        set
-        {
-            _currentAnimation = value;
-            UpdateAnimations();
-        }
+      var suffix = CurrentAnimation.ToString();
+      return suffix;
     }
-    public string AnimationName
+  }
+  private void UpdateAnimations()
+  {
+    var animation = AnimationName;
+    Animator.Play(animation);
+  }
+
+  void Awake()
+  {
+    Health = GetComponent<Health>();
+    Health.OnHit += OnHit;
+    Health.OnDeath += OnDeath;
+    FacingController = GetComponent<FacingController>();
+    Animator = GetComponent<Animator>();
+    Flash = GetComponent<Flash>();
+  }
+
+  private void OnDeath(Health health)
+  {
+    gameObject.SetActive(false);
+  }
+
+  private void OnHit(Health health)
+  {
+    Flash.StartFlash();
+  }
+
+  void Update()
+  {
+
+    if (Input.GetMouseButtonDown(0))
     {
-        get
-        {
-            var suffix = CurrentAnimation.ToString();
-            return suffix;
-        }
-    }
-    private void UpdateAnimations()
-    {
-        var animation = AnimationName;
-        Animator.Play(animation);
-    }
-
-    void Awake()
-    {
-        Health = GetComponent<Health>();
-        Health.OnHit += OnHit;
-        Health.OnDeath += OnDeath;
-        FacingController = GetComponent<FacingController>();
-        Animator = GetComponent<Animator>();
-        Flash = GetComponent<Flash>();
-    }
-
-    private void OnDeath(Health health)
-    {
-        gameObject.SetActive(false);
-    }
-
-    private void OnHit(Health health)
-    {
-        Flash.StartFlash();
-    }
-
-    void Update()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            CurrentAnimation = Animation.Attack_BT;
-
-        }
-        // si animation terminee reset currentanimation
-        if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
-            CurrentAnimation = Animation.Idle;
-
-        if (CurrentAnimation != Animation.Attack_BT)
-        {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            if (horizontal != 0.0f || vertical != 0.0f)
-            {
-                Animator.SetFloat("FacingX", horizontal);
-                Animator.SetFloat("FacingY", vertical);
-                CurrentAnimation = Animation.Walk;
-            }
-            else
-            {
-                CurrentAnimation = Animation.Idle;
-            }
-        }
+      CurrentAnimation = Animation.Attack_BT;
 
     }
+    // si animation terminee reset currentanimation
+    if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+      CurrentAnimation = Animation.Idle;
+
+    if (CurrentAnimation != Animation.Attack_BT)
+    {
+      float horizontal = Input.GetAxisRaw("Horizontal");
+      float vertical = Input.GetAxisRaw("Vertical");
+      if (horizontal != 0.0f || vertical != 0.0f)
+      {
+        Animator.SetFloat("FacingX", horizontal);
+        Animator.SetFloat("FacingY", vertical);
+        CurrentAnimation = Animation.Walk;
+      }
+      else
+      {
+        CurrentAnimation = Animation.Idle;
+      }
+    }
+
+  }
+
+  private void OnTriggerStay2D(Collider2D collider)
+  {
+    var playerPosition = gameObject.GetComponent<BoxCollider2D>().bounds.min.y;
+    Debug.Log("PLAYER HAS COLLIDED");
+
+    /*if (collider.CompareTag("Ledge"))
+    {
+
+      var ledgePosition = collider.bounds.max.y;
+      if (playerPosition > ledgePosition)
+      {
+        var ledgeHeight = collider.bounds.size.y;
+        Debug.Log(ledgeHeight);
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - ledgeHeight);
+      }
+
+
+    }*/
+  }
 }
