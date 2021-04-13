0b510ef5f135f41a5dd6f5f8ffa4c4fd853e41f1
Caroline Emond Serret <caroline.emond.serret@gmail.com>
2021-04-13 15:47:00 -0400

    Added ledge colliders, started collision detection for player

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,75 +3,97 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public enum Animation
-    {
-        Idle_Down,
-        Walk,
-        Attack,
-    }
+  public enum Animation
+  {
+    Idle_Down,
+    Walk,
+    Attack,
+  }
 
-    public Health Health { get; private set; }
-    public FacingController FacingController;
-    public Animator Animator;
-    private Animation _currentAnimation;
+  public Health Health { get; private set; }
+  public FacingController FacingController;
+  public Animator Animator;
+  private Animation _currentAnimation;
+  private BoxCollider2D playerCollider;
 
-    public Animation CurrentAnimation
+  public Animation CurrentAnimation
+  {
+    get { return _currentAnimation; }
+    set
     {
-        get { return _currentAnimation; }
-        set
-        {
-            _currentAnimation = value;
-            UpdateAnimations();
-        }
+      _currentAnimation = value;
+      UpdateAnimations();
     }
-    public string AnimationName
+  }
+  public string AnimationName
+  {
+    get
     {
-        get
-        {
-            var suffix = CurrentAnimation.ToString();
-            return suffix;
-        }
+      var suffix = CurrentAnimation.ToString();
+      return suffix;
     }
-    private void UpdateAnimations()
-    {
-        var animation = AnimationName;
-        Animator.Play(animation);
-    }
+  }
+  private void UpdateAnimations()
+  {
+    var animation = AnimationName;
+    Animator.Play(animation);
+  }
 
-    void Awake()
+  void Awake()
+  {
+    Health = GetComponent<Health>();
+    Health.OnHit += OnHit;
+    Health.OnDeath += OnDeath;
+    FacingController = GetComponent<FacingController>();
+    Animator = GetComponent<Animator>();
+    playerCollider = GameManager.Instance.Player.GetComponent<BoxCollider2D>();
+  }
+
+  private void OnDeath(Health health)
+  {
+    gameObject.SetActive(false);
+  }
+
+  private void OnHit(Health health)
+  {
+    throw new NotImplementedException();
+  }
+
+  void Update()
+  {
+
+    if (gameObject.GetComponent<Rigidbody2D>().velocity.x != 0.0f || gameObject.GetComponent<Rigidbody2D>().velocity.y != 0.0f)
     {
-        Health = GetComponent<Health>();
-        Health.OnHit += OnHit;
-        Health.OnDeath += OnDeath;
-        FacingController = GetComponent<FacingController>();
-        Animator = GetComponent<Animator>();
-    }
+      float horizontal = Input.GetAxisRaw("Horizontal");
+      float vertical = Input.GetAxisRaw("Vertical");
+      Animator.SetFloat("FacingX", horizontal);
+      Animator.SetFloat("FacingY", vertical);
+      CurrentAnimation = Animation.Walk;
 
-    private void OnDeath(Health health)
+    }
+    else
     {
-        gameObject.SetActive(false);
+      CurrentAnimation = Animation.Idle_Down;
     }
+  }
 
-    private void OnHit(Health health)
+  private void OnTriggerEnter2D(Collider2D collider)
+  {
+    var playerPosition = playerCollider.bounds.min.y;
+    Debug.Log(collider);
+
+    if (collider.CompareTag("Ledge"))
     {
-        throw new NotImplementedException();
+
+      var ledgePosition = collider.bounds.max.y;
+      if (playerPosition > ledgePosition)
+      {
+        var ledgeHeight = collider.bounds.size.y;
+        Debug.Log(ledgeHeight);
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - ledgeHeight);
+      }
+
+
     }
-
-    void Update()
-    {
-
-        if (gameObject.GetComponent<Rigidbody2D>().velocity.x != 0.0f || gameObject.GetComponent<Rigidbody2D>().velocity.y != 0.0f)
-        {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            Animator.SetFloat("FacingX", horizontal);
-            Animator.SetFloat("FacingY", vertical);
-            CurrentAnimation = Animation.Walk;
-
-        }
-        else
-        {
-            CurrentAnimation = Animation.Idle_Down;
-        }
-    }
+  }
 }
