996b1c86d6d14e4d17257a58944f32d5bf6803b0
Francois <flapierre101@gmail.com>
2021-04-20 17:55:14 -0400

    Destructable drop consumables

--- Assets/Script/DestructableBarrel.cs
+++ Assets/Script/DestructableBarrel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +8,10 @@ public class DestructableBarrel : MonoBehaviour, IDestructable
     //private Player player;
     public Health Health { get; private set; }
     public Animator Animator { get; private set; }
+    private int itemSpawn; 
 
     void Awake()
     {
-        //player = GameManager.Instance.Player;
         Animator = GetComponent<Animator>();
         Animator.enabled = false;
         Health = GetComponent<Health>();
@@ -27,7 +27,22 @@ public class DestructableBarrel : MonoBehaviour, IDestructable
     private void Update()
     {
         if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && Health.Value == 0)
+        {
             Destroy(gameObject);
+            itemSpawn = Random.Range(1, 3);
+
+            switch (itemSpawn)
+            {
+                case 1: GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.Consumable_BluePotion, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case 2: GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.Consumable_Heart, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case 3: GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.Consumable_RedPotion, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
 }
--- Assets/Script/PrefabManager.cs
+++ Assets/Script/PrefabManager.cs
@@ -4,7 +4,13 @@ public class PrefabManager : MonoBehaviour
 {
     public enum Global
     {
+        Consumable_BluePotion,
+        Consumable_Heart,
+        Consumable_RedPotion,
         Destructable_Barrel,
+        Destructable_Bush,
+        Destructable_MetalCrate,
+        Destructable_WoodCrate,
         OldMan,
         Player,
 
