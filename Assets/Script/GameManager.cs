2e3b1c666d9bc4dc3c5001caf6d875779b53d751
Dany Viens <dany.viens@gmail.com>
2021-04-29 13:23:53 -0400

    Return to menu on death

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,110 +5,117 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-  private static GameManager _instance;
-  public static GameManager Instance
-  {
-    get
+    private static GameManager _instance;
+    public static GameManager Instance
     {
-      // TODO: use a bootloader instead to create this before level is started since it can be expensive to load all assets
-      if (_instance == null)
-      {
-        var gameManagerGameObject = Resources.Load<GameObject>("prefabs/GameManager");
-        var managerObject = Instantiate(gameManagerGameObject);
-        _instance = managerObject.GetComponent<GameManager>();
-        _instance.Initialize();
+        get
+        {
+            // TODO: use a bootloader instead to create this before level is started since it can be expensive to load all assets
+            if (_instance == null)
+            {
+                var gameManagerGameObject = Resources.Load<GameObject>("prefabs/GameManager");
+                var managerObject = Instantiate(gameManagerGameObject);
+                _instance = managerObject.GetComponent<GameManager>();
+                _instance.Initialize();
 
-        // Prevents having to recreate the manager on scene change
-        // https://docs.unity3d.com/ScriptReference/Object.DontDestroyOnLoad.html
-        DontDestroyOnLoad(_instance);
-      }
+                // Prevents having to recreate the manager on scene change
+                // https://docs.unity3d.com/ScriptReference/Object.DontDestroyOnLoad.html
+                DontDestroyOnLoad(_instance);
+            }
 
-      return _instance;
-    }
-  }
-
-  public PrefabManager PrefabManager { get; private set; }
-  public SoundManager SoundManager { get; private set; }
-  public LevelManager LevelManager { get; private set; }
-
-  public SavegameManager SavegameManager { get; private set; }
-  public UIManager UIManager { get; private set; }
-
-  public DialogueManager DialogueManager { get; private set; }
-
-  public Player Player { get; private set; }
-  public Level Level { get; private set; }
-  public Camera Camera { get; private set; }
-  public Plane[] FrustumPlanes { get; private set; }
-
-  private void Initialize()
-  {
-    SoundManager = GetComponentInChildren<SoundManager>();
-    UIManager = GetComponentInChildren<UIManager>();
-    PrefabManager = GetComponentInChildren<PrefabManager>();
-    LevelManager = GetComponentInChildren<LevelManager>();
-    SavegameManager = GetComponentInChildren<SavegameManager>();
-    DialogueManager = GetComponentInChildren<DialogueManager>();
-
-    SceneManager.sceneLoaded += OnSceneLoaded;
-
-    OnSceneLoaded();
-  }
-
-  private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-  {
-    OnSceneLoaded();
-  }
-
-  private void OnSceneLoaded()
-  {
-    Player = FindObjectOfType<Player>();
-    Level = FindObjectOfType<Level>();
-    Camera = FindObjectOfType<Camera>();
-
-    // Turning off a single layer by code
-    //Camera.cullingMask &= ~(1 << LayerMask.NameToLayer("EnemyHitbox"));
-
-    // Dynamically create Player in the scene
-
-    if (!Player)
-    {
-      Player = FindObjectOfType<Player>();
-
-      if (!Player)
-      {
-        var playerGameObject = PrefabManager.Spawn(PrefabManager.Global.Player, Vector3.zero);
-        Player = playerGameObject.GetComponent<Player>();
-        DontDestroyOnLoad(Player);
-      }
+            return _instance;
+        }
     }
 
-    LevelManager.OnLevelStart();
-  }
+    public PrefabManager PrefabManager { get; private set; }
+    public SoundManager SoundManager { get; private set; }
+    public LevelManager LevelManager { get; private set; }
 
-  private void Update()
-  {
-    FrustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+    public SavegameManager SavegameManager { get; private set; }
+    public UIManager UIManager { get; private set; }
+
+    public DialogueManager DialogueManager { get; private set; }
+
+    public Player Player { get; private set; }
+    public Level Level { get; private set; }
+    public Camera Camera { get; private set; }
+    public Plane[] FrustumPlanes { get; private set; }
+
+    private void Initialize()
+    {
+        SoundManager = GetComponentInChildren<SoundManager>();
+        UIManager = GetComponentInChildren<UIManager>();
+        PrefabManager = GetComponentInChildren<PrefabManager>();
+        LevelManager = GetComponentInChildren<LevelManager>();
+        SavegameManager = GetComponentInChildren<SavegameManager>();
+        DialogueManager = GetComponentInChildren<DialogueManager>();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        OnSceneLoaded();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        OnSceneLoaded();
+    }
+
+    private void OnSceneLoaded()
+    {
+        Player = FindObjectOfType<Player>();
+        Level = FindObjectOfType<Level>();
+        Camera = FindObjectOfType<Camera>();
+
+        // Turning off a single layer by code
+        //Camera.cullingMask &= ~(1 << LayerMask.NameToLayer("EnemyHitbox"));
+
+        // Dynamically create Player in the scene
+
+        if (!Player)
+        {
+            Player = FindObjectOfType<Player>();
+
+            if (!Player)
+            {
+                var playerGameObject = PrefabManager.Spawn(PrefabManager.Global.Player, Vector3.zero);
+                Player = playerGameObject.GetComponent<Player>();
+                DontDestroyOnLoad(Player);
+            }
+        }
+
+        LevelManager.OnLevelStart();
+    }
+
+    private void Update()
+    {
+        FrustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
 
 
-  }
+    }
 
-  public void RestartLevel()
-  {
-    LevelManager.GoToLevel(LevelManager.CurrentLevel, LevelManager.LevelEntranceId);
-    Player.OnLevelRestart();
-  }
+    public void RestartLevel()
+    {
+        LevelManager.GoToLevel(LevelManager.CurrentLevel, LevelManager.LevelEntranceId);
+        Player.OnLevelRestart();
+    }
 
-  public bool IsInsideCamera(Renderer renderer)
-  {
-    if (GeometryUtility.TestPlanesAABB(FrustumPlanes, renderer.bounds))
-      return true;
+    public bool IsInsideCamera(Renderer renderer)
+    {
+        if (GeometryUtility.TestPlanesAABB(FrustumPlanes, renderer.bounds))
+            return true;
 
-    return false;
-  }
+        return false;
+    }
 
-  public void bootstrap()
-  {
-    OnSceneLoaded();
-  }
+    public void bootstrap()
+    {
+        OnSceneLoaded();
+    }
+
+    public void destroyInstance()
+    {
+        Destroy(Instance);
+        Destroy(GameObject.Find("GameManager(Clone)"));
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,242 +1,254 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-  public enum Animation
-  {
-    Idle,
-    Walk,
-    Attack_BT,
-  }
-
-  public Health Health { get; private set; }
-  public Mana Mana { get; private set; }
-  public Transform SlashSpawn;
-  private Animator Animator;
-  private Animation _currentAnimation;
-  private Flash Flash;
-  private GameManager instance;
-  private INPCBehaviour npc;
-  private float npcTimer;
-
-
-  public Animation CurrentAnimation
-  {
-    get { return _currentAnimation; }
-    set
+    public enum Animation
     {
-      _currentAnimation = value;
-      UpdateAnimations();
-    }
-  }
-  public string AnimationName
-  {
-    get
-    {
-      var suffix = CurrentAnimation.ToString();
-      return suffix;
-    }
-  }
-  private void UpdateAnimations()
-  {
-    var animation = AnimationName;
-    Animator.Play(animation);
-  }
-
-  void Awake()
-  {
-    instance = GameManager.Instance;
-    Health = GetComponent<Health>();
-    Mana = GetComponent<Mana>();
-    Health.OnHit += OnHit;
-    Health.OnDeath += OnDeath;
-    Health.OnChanged += OnChanged;
-    Mana.OnChanged += OnChangedMana;
-    Animator = GetComponent<Animator>();
-    Flash = GetComponent<Flash>();
-    npcTimer = 0.0f;
-  }
-
-
-  private void OnDeath(Health health)
-  {
-    gameObject.SetActive(false);
-  }
-
-  private void OnHit(Health health)
-  {
-    Flash.StartFlash();
-
-  }
-
-  private void OnChanged(Health health)
-  {
-    instance.UIManager.updateHeart();
-  }
-
-  private void OnChangedMana(Mana mana)
-  {
-    instance.UIManager.updateMana();
-  }
-
-  private void OnUse(Mana mana)
-  {
-    mana.Value--;
-  }
-
-  void Update()
-  {
-    if (npcTimer > 0.0f)
-    {
-      npcTimer = npcTimer - Time.deltaTime;
+        Idle,
+        Walk,
+        Attack_BT,
     }
 
-    if (Input.GetMouseButtonDown(0) && instance.SavegameManager.saveData.hasSword)
+    public Health Health { get; private set; }
+    public Mana Mana { get; private set; }
+    public Transform SlashSpawn;
+    private Animator Animator;
+    private Animation _currentAnimation;
+    private Flash Flash;
+    private GameManager instance;
+    private INPCBehaviour npc;
+    private float npcTimer;
+
+
+    public Animation CurrentAnimation
     {
-      CurrentAnimation = Animation.Attack_BT;
-      Animator.Update(0);
-      if (Animator.GetFloat("FacingX") == 1)
-      {
-        if (instance.SavegameManager.saveData.hasMasterSword)
-          GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashRight, SlashSpawn.position, SlashSpawn.rotation);
+        get { return _currentAnimation; }
+        set
+        {
+            _currentAnimation = value;
+            UpdateAnimations();
+        }
+    }
+    public string AnimationName
+    {
+        get
+        {
+            var suffix = CurrentAnimation.ToString();
+            return suffix;
+        }
+    }
+    private void UpdateAnimations()
+    {
+        var animation = AnimationName;
+        Animator.Play(animation);
+    }
+
+    void Awake()
+    {
+        instance = GameManager.Instance;
+        Health = GetComponent<Health>();
+        Mana = GetComponent<Mana>();
+        Health.OnHit += OnHit;
+        Health.OnDeath += OnDeath;
+        Health.OnChanged += OnChanged;
+        Mana.OnChanged += OnChangedMana;
+        Animator = GetComponent<Animator>();
+        Flash = GetComponent<Flash>();
+        npcTimer = 0.0f;
+    }
+
+
+    private void OnDeath(Health health)
+    {
+        GameManager.Instance.SoundManager.Play(SoundManager.Music.Gameover);
+        StartCoroutine(LoadLevelAfterDelay(3));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameManager.Instance.SavegameManager.onDeath();
+        GameManager.Instance.destroyInstance();
+        GameManager.Instance.SoundManager.Stop(SoundManager.Music.Gameover);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnHit(Health health)
+    {
+        Flash.StartFlash();
+
+    }
+
+    private void OnChanged(Health health)
+    {
+        instance.UIManager.updateHeart();
+    }
+
+    private void OnChangedMana(Mana mana)
+    {
+        instance.UIManager.updateMana();
+    }
+
+    private void OnUse(Mana mana)
+    {
+        mana.Value--;
+    }
+
+    void Update()
+    {
+        if (npcTimer > 0.0f)
+        {
+            npcTimer = npcTimer - Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0) && instance.SavegameManager.saveData.hasSword)
+        {
+            CurrentAnimation = Animation.Attack_BT;
+            Animator.Update(0);
+            if (Animator.GetFloat("FacingX") == 1)
+            {
+                if (instance.SavegameManager.saveData.hasMasterSword)
+                    GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashRight, SlashSpawn.position, SlashSpawn.rotation);
+                else
+                    GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashRight, SlashSpawn.position, SlashSpawn.rotation);
+            }
+            else if (Animator.GetFloat("FacingX") == -1)
+            {
+                if (instance.SavegameManager.saveData.hasMasterSword)
+                    GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashLeft, SlashSpawn.position, SlashSpawn.rotation);
+                GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashLeft, SlashSpawn.position, SlashSpawn.rotation);
+            }
+            else if (Animator.GetFloat("FacingY") == -1)
+            {
+                if (instance.SavegameManager.saveData.hasMasterSword)
+                    //GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashDown, SlashSpawn.position, SlashSpawn.rotation);
+                    GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashDown, SlashSpawn.position, SlashSpawn.rotation);
+            }
+            else if (Animator.GetFloat("FacingY") == 1)
+            {
+                if (instance.SavegameManager.saveData.hasMasterSword)
+                    //GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashUp, SlashSpawn.position, SlashSpawn.rotation);
+                    GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashUp, SlashSpawn.position, SlashSpawn.rotation);
+            }
+
+
+        }
+        // si animation terminee reset currentanimation
+        if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+            CurrentAnimation = Animation.Idle;
+
+        if (CurrentAnimation != Animation.Attack_BT)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            if (horizontal != 0.0f || vertical != 0.0f)
+            {
+                Animator.SetFloat("FacingX", horizontal);
+                Animator.SetFloat("FacingY", vertical);
+                CurrentAnimation = Animation.Walk;
+            }
+            else
+            {
+                CurrentAnimation = Animation.Idle;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (npc != null)
+            {
+                npc.UpdateBehaviour();
+
+            }
+
+        }
+        // Pour debug le heart UI
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            Health.Value -= 1;
+        }
+        // Pour debug le heart UI
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            Health.Value += 1;
+        }
+        // Pour debug le mana UI
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            Mana.Value -= 1;
+        }
+        // Pour debug le mana UI
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            Mana.Value += 1;
+        }
+        // Debug : Ajout de l'épée
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            instance.SavegameManager.saveData.hasSword = true;
+            instance.SavegameManager.saveData.equipedWeapon = SaveData.EquipedWeapon.Sword;
+            instance.UIManager.updateWeapon();
+        }
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            instance.SavegameManager.saveData.hasMasterSword = true;
+            instance.SavegameManager.saveData.equipedWeapon = SaveData.EquipedWeapon.MasterSword;
+            instance.UIManager.updateWeapon();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        INPCBehaviour Inpc = other.GetComponentInParent<INPCBehaviour>();
+        if (Inpc != null)
+        {
+            if (npcTimer <= 0.0f)
+            {
+                instance.SoundManager.Play(SoundManager.Sfx.heyListen);
+                npcTimer = 5.0f;
+
+            }
+            npc = Inpc;
+
+        }
+
+
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Oldman oldman = other.GetComponentInParent<Oldman>();
+        if (oldman != null)
+        {
+            npc = null;
+        }
+    }
+
+    public void OnLevelStart(LevelEntrance levelEntrance)
+    {
+        if (levelEntrance != null)
+        {
+            transform.position = levelEntrance.transform.position;
+        }
         else
-          GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashRight, SlashSpawn.position, SlashSpawn.rotation);
-      }
-      else if (Animator.GetFloat("FacingX") == -1)
-      {
-        if (instance.SavegameManager.saveData.hasMasterSword)
-          GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashLeft, SlashSpawn.position, SlashSpawn.rotation);
-        GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashLeft, SlashSpawn.position, SlashSpawn.rotation);
-      }
-      else if (Animator.GetFloat("FacingY") == -1)
-      {
-        if (instance.SavegameManager.saveData.hasMasterSword)
-          //GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashDown, SlashSpawn.position, SlashSpawn.rotation);
-          GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashDown, SlashSpawn.position, SlashSpawn.rotation);
-      }
-      else if (Animator.GetFloat("FacingY") == 1)
-      {
-        if (instance.SavegameManager.saveData.hasMasterSword)
-          //GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.MasterSlashUp, SlashSpawn.position, SlashSpawn.rotation);
-          GameManager.Instance.PrefabManager.Instanciate(PrefabManager.Global.SlashUp, SlashSpawn.position, SlashSpawn.rotation);
-      }
+        {
+            transform.position = Vector3.zero;
+        }
 
-
+        //PlatformController.Reset();
     }
-    // si animation terminee reset currentanimation
-    if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
-      CurrentAnimation = Animation.Idle;
-
-    if (CurrentAnimation != Animation.Attack_BT)
+    public void OnLevelRestart()
     {
-      float horizontal = Input.GetAxisRaw("Horizontal");
-      float vertical = Input.GetAxisRaw("Vertical");
-      if (horizontal != 0.0f || vertical != 0.0f)
-      {
-        Animator.SetFloat("FacingX", horizontal);
-        Animator.SetFloat("FacingY", vertical);
-        CurrentAnimation = Animation.Walk;
-      }
-      else
-      {
-        CurrentAnimation = Animation.Idle;
-      }
+        GameManager.Instance.Camera.GetComponent<FollowObject>().TargetTransform = gameObject.transform;
     }
 
-    if (Input.GetKeyUp(KeyCode.E))
+    private void Start()
     {
-      if (npc != null)
-      {
-        npc.UpdateBehaviour();
-
-      }
-
+        OnLevelRestart();
     }
-    // Pour debug le heart UI
-    if (Input.GetKeyUp(KeyCode.H))
-    {
-      Health.Value -= 1;
-    }
-    // Pour debug le heart UI
-    if (Input.GetKeyUp(KeyCode.J))
-    {
-      Health.Value += 1;
-    }
-    // Pour debug le mana UI
-    if (Input.GetKeyUp(KeyCode.N))
-    {
-      Mana.Value -= 1;
-    }
-    // Pour debug le mana UI
-    if (Input.GetKeyUp(KeyCode.M))
-    {
-      Mana.Value += 1;
-    }
-    // Debug : Ajout de l'épée
-    if (Input.GetKeyUp(KeyCode.K))
-    {
-      instance.SavegameManager.saveData.hasSword = true;
-      instance.SavegameManager.saveData.equipedWeapon = SaveData.EquipedWeapon.Sword;
-      instance.UIManager.updateWeapon();
-    }
-    if (Input.GetKeyUp(KeyCode.L))
-    {
-      instance.SavegameManager.saveData.hasMasterSword = true;
-      instance.SavegameManager.saveData.equipedWeapon = SaveData.EquipedWeapon.MasterSword;
-      instance.UIManager.updateWeapon();
-    }
-  }
-
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-
-    INPCBehaviour Inpc = other.GetComponentInParent<INPCBehaviour>();
-    if (Inpc != null)
-    {
-      if (npcTimer <= 0.0f)
-      {
-        instance.SoundManager.Play(SoundManager.Sfx.heyListen);
-        npcTimer = 5.0f;
-
-      }
-      npc = Inpc;
-
-    }
-
-
-
-  }
-
-
-  private void OnTriggerExit2D(Collider2D other)
-  {
-    Oldman oldman = other.GetComponentInParent<Oldman>();
-    if (oldman != null)
-    {
-      npc = null;
-    }
-  }
-
-  public void OnLevelStart(LevelEntrance levelEntrance)
-  {
-    if (levelEntrance != null)
-    {
-      transform.position = levelEntrance.transform.position;
-    }
-    else
-    {
-      transform.position = Vector3.zero;
-    }
-
-    //PlatformController.Reset();
-  }
-  public void OnLevelRestart()
-  {
-    GameManager.Instance.Camera.GetComponent<FollowObject>().TargetTransform = gameObject.transform;
-  }
-
-  private void Start()
-  {
-    OnLevelRestart();
-  }
 }
