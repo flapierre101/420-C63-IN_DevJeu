d23cbab83b446261df4d00e29d013dfdd68c4d16
Caroline Emond Serret <caroline.emond.serret@gmail.com>
2021-05-06 10:55:37 -0400

    Started Sorcerer and electro ball spell

---
+++ Assets/Script/ElectroBall.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class ElectroBall : MonoBehaviour
+{
+  public enum Animation
+  {
+    Cast,
+    Impact,
+  }
+
+  private Animation _currentAnimation;
+
+  public Animation CurrentAnimation
+  {
+    get { return _currentAnimation; }
+    set
+    {
+      _currentAnimation = value;
+      UpdateAnimations();
+    }
+  }
+  public string AnimationName
+  {
+    get
+    {
+      var suffix = CurrentAnimation.ToString();
+      return "ElectroBall_" + suffix;
+    }
+  }
+
+  private void UpdateAnimations()
+  {
+    var animation = AnimationName;
+    Animator.Play(animation);
+  }
+  public BoxCollider2D projectileCollider;
+  public Animator Animator;
+  public int Damage = 1;
+
+  private void Awake()
+  {
+    projectileCollider = GetComponent<BoxCollider2D>();
+    Animator = GetComponent<Animator>();
+    CurrentAnimation = Animation.Cast;
+
+  }
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    projectileCollider.enabled = false;
+    CurrentAnimation = Animation.Impact;
+    Fade fade = gameObject.GetComponent<Fade>();
+    fade.FadeWaitTime = 0.75f;
+    fade.StartFade();
+
+    Health playerHealth = collision.GetComponent<Health>();
+
+    playerHealth.Value -= Damage;
+
+  }
+}
---
+++ Assets/Script/Sorcerer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Sorcerer : MonoBehaviour
+{
+  public BoxCollider2D SorcererCollider;
+  public Animator Animator;
+  public SpriteRenderer SpriteX;
+  public Health SorcererHealth;
+  public FacingController FacingController;
+  public Transform SpawnPoint;
+
+
+
+
+  private void Awake()
+  {
+    SorcererCollider = GetComponent<BoxCollider2D>();
+    Animator = GetComponent<Animator>();
+    SpriteX = GetComponent<SpriteRenderer>();
+    SorcererHealth = GetComponent<Health>();
+    FacingController = GetComponent<FacingController>();
+  }
+
+  void Update()
+  {
+
+  }
+}
